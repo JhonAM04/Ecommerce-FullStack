aa087395ef978d1c636e27562977d3b71044d108
JhonAM04 <Jhonalvarez_01@outlook.com>
2025-07-30 00:06:09 -0500

    Backend: Se agrego un AllowedHost

--- back/app/settings.py
+++ back/app/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-j2e-#k7)60^2s#med!6nd^2y6qzb7aqy*vt0o9fy&s$clv=(@-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ecommerce-fullstack-wfxr.onrender.com']
 
 
 # Application definition
